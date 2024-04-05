525151ae95b90bf3dbc202e42d240198693fb15f
Molikverma <163959014+Molikverma@users.noreply.github.com>
2024-04-04 21:18:06 -0700

    Create Sortarray.java

---
+++ Sortarray.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class SortableArray {
+    public static void main(String[] args) {
+        // Sort an array of integers
+        int[] intArray = {4, 2, 7, 1, 9, 5};
+        System.out.println("Original Integer Array:");
+        printIntArray(intArray);
+        Arrays.sort(intArray);
+        System.out.println("\nSorted Integer Array:");
+        printIntArray(intArray);
+
+        // Sort an array of strings
+        String[] strArray = {"banana", "apple", "orange", "grape"};
+        System.out.println("\nOriginal String Array:");
+        printStrArray(strArray);
+        Arrays.sort(strArray);
+        System.out.println("\nSorted String Array:");
+        printStrArray(strArray);
+    }
+
+    public static void printIntArray(int[] array) {
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void printStrArray(String[] array) {
+        for (String str : array) {
+            System.out.print(str + " ");
+        }
+        System.out.println();
+    }
+}
